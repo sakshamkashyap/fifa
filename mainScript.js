b2b22b9c2de2ca8e1cb9b86cff2185125111091f
Saksham <sakshamkashyap@hotmail.com>
2021-04-06 16:16:41 -0700

    Added calls to firestore

--- mainScript.js
+++ mainScript.js
@@ -1,3 +1,12 @@
+var db = null;
+
+function initDB() {
+    if(db == null) {
+        db = connectToDB();
+    }
+    return db;
+}
+
 function displayMode() {
     if (document.getElementById('radioFindTnmt').checked) {
         document.getElementById('findTnmt').style.display = 'initial';
@@ -33,9 +42,64 @@ function addFields() {
 
 }
 
-
 function createTournament() {
     document.getElementById('screen-1').style.display = 'none';
     document.getElementById('screen-2').style.display = 'initial';
-    console.log("OK");
+    addTournament();
+}
+
+function addTournament() {
+    var radios = document.getElementsByName('type-leg');
+    var leg = 1;
+    var bonus = document.getElementById('bonus').value;
+    var teams = document.getElementById('numOfTeams').value;
+    var eventId = 0;
+
+    for(var i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+            radios[i].value;
+            break;
+        }
+    }
+
+    db.collection("events")
+    .orderBy('eventId', 'desc')
+    .limit(1).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            eventId = doc.data().eventId + 1;
+
+            db.collection("events").add({
+                eventId: eventId,
+                win: 3,
+                loss: 0,
+                draw: 1,
+                legs: Number(leg),
+                roundWin: Number(bonus),
+                teams: Number(teams)
+            })
+            .then((docRef) => {
+                console.log("Collection created: " + docRef.id);
+            })
+            .catch((error) => {
+                console.log("Error during collection creation: " + error);
+            });
+        });
+    });
+}
+
+function connectToDB() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCdwHEqvLzSWaHrqkHSchHwyVxz4LdsBck",
+        authDomain: "fifa-tnmt.firebaseapp.com",
+        databaseURL: "https://fifa-tnmt-default-rtdb.firebaseio.com",
+        projectId: "fifa-tnmt",
+        storageBucket: "fifa-tnmt.appspot.com",
+        messagingSenderId: "546072301727",
+        appId: "1:546072301727:web:5d80f00f01bf1ae9a6d2aa",
+        measurementId: "G-SVJ4T3XXT7"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+      return firebase.firestore();
 }
